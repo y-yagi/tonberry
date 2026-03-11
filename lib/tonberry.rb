88f02d5fe301fa4a9a22aea48503d02318545095
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2026-03-12 07:47:11 +0900

    Extract agent releated operations to `Agent` class

--- lib/tonberry.rb
+++ lib/tonberry.rb
@@ -3,50 +3,10 @@
 require "rainbow"
 require "reline"
 require_relative "tonberry/version"
-require_relative "tonberry/client"
+require_relative "tonberry/agent"
 
 module Tonberry
   def self.run(args)
-    client = Tonberry::Client.new(cost_limit_in_dollars: 0.1)
-
-    begin
-      while line = Reline.readline(Rainbow("> ").bright, true)
-        case line.chomp
-        when "exit", "quit", "q"
-          $stdout.puts Rainbow("bye!").bright.green
-          exit 0
-        when ""
-          # Do nothing
-        else
-          begin
-            result = nil
-            spinner_thread = Thread.new do
-              spinner_chars = %w[⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏]
-              i = 0
-              while result.nil?
-                $stdout.print Rainbow("\r#{spinner_chars[i % spinner_chars.size]} Processing...").green
-                $stdout.flush
-                sleep 0.5
-                i += 1
-              end
-              $stdout.print "\r\e[K"
-              $stdout.flush
-            end
-
-            result = client.chat(line)
-            spinner_thread.join
-
-            $stdout.puts Rainbow(result).bright.green
-          rescue CostLimitExceededError => e
-            result = :error
-            spinner_thread&.join
-            $stdout.puts Rainbow(e.message).bright.red
-          end
-        end
-      end
-    rescue Interrupt, EOFError
-      $stdout.puts Rainbow("bye!").bright.green
-      exit 0
-    end
+    Agent.new(args).run
   end
 end
---
+++ lib/tonberry/agent.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require_relative "client"
+
+module Tonberry
+  class Agent
+    def initialize(args)
+      @client = Tonberry::Client.new(cost_limit_in_dollars: 0.1)
+    end
+
+    def run
+      begin
+        while line = Reline.readline(Rainbow("> ").bright, true)
+          process_line(line.chomp)
+        end
+      rescue Interrupt, EOFError
+        exit_gracefully
+      end
+    end
+
+    private
+
+    def process_line(line)
+      case line
+      when "exit", "quit", "q"
+        exit_gracefully
+      when ""
+        # Do nothing
+      else
+        chat_with_spinner(line)
+      end
+    end
+
+    def chat_with_spinner(line)
+      result = nil
+      spinner_thread = start_spinner { result }
+
+      begin
+        result = @client.chat(line)
+        spinner_thread.join
+        $stdout.puts Rainbow(result).bright.green
+      rescue CostLimitExceededError => e
+        result = :error
+        spinner_thread.join
+        $stdout.puts Rainbow(e.message).bright.red
+      end
+    end
+
+    def start_spinner(&result_check)
+      Thread.new do
+        spinner_chars = %w[⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏]
+        i = 0
+        while result_check.call.nil?
+          $stdout.print Rainbow("\r#{spinner_chars[i % spinner_chars.size]} Processing...").green
+          $stdout.flush
+          sleep 0.5
+          i += 1
+        end
+        $stdout.print "\r\e[K"
+        $stdout.flush
+      end
+    end
+
+    def exit_gracefully
+      $stdout.puts Rainbow("bye!").bright.green
+      exit 0
+    end
+  end
+end
