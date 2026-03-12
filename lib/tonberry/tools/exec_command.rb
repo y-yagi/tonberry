b458ce12e04e6fd04fa6ccea3ab37ad42ad1be73
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2026-03-13 07:31:21 +0900

    Limit to runnable commands

--- lib/tonberry/tools/exec_command.rb
+++ lib/tonberry/tools/exec_command.rb
@@ -5,8 +5,10 @@ require "open3"
 module Tonberry
   module Tools
     module ExecCommand
+      ALLOWED_COMMANDS = %w[git ls].freeze
+
       class InputSchema < Anthropic::BaseModel
-        required :command, String, doc: "The command to run"
+        required :command, String, doc: "The command to run (only 'git' and 'ls' are allowed)"
 
         doc "Run the given command in local"
       end
@@ -15,6 +17,11 @@ module Tonberry
         input_schema InputSchema
 
         def call(command:)
+          executable = command.split.first
+          unless ALLOWED_COMMANDS.include?(executable)
+            return "Error: '#{executable}' is not allowed. Only the following commands are allowed: #{ALLOWED_COMMANDS.join(", ")}"
+          end
+
           output, _ = Open3.capture2e(command)
           output
         end
